6ed50f201dc587b2a281e7a1b0db675a767d909c
Waynexia888 <qiwuwaynexia@gmail.com>
2022-03-10 19:37:15 -0800

    15. First Occurrence & recusrive way

--- LaiCode/Binary Search/15. First Occurrence.java
+++ LaiCode/Binary Search/15. First Occurrence.java
@@ -42,3 +42,66 @@ public class Solution {
 
 // time: O(logn)
 // space: O(1)
+
+//------------------------------Recursion Way ------------------------------------
+public class Solution {
+    /**
+     * @param nums: The integer array.
+     * @param target: Target to find.
+     * @return: The first position of target. Position starts from 0.
+     */
+    public int binarySearch(int[] nums, int target) {
+      // write your code here
+      return binarySearch(nums, 0, nums.length - 1, target);
+    }
+
+    private int binarySearch(int[] nums, int start, int end, int target) {
+      // base case 
+      if (start > end) {
+        return -1;
+      }
+
+      int mid = (start + end) / 2;
+      if (nums[mid] == target) {
+        if (mid >= 1 && nums[mid] == nums[mid - 1]) {
+          return binarySearch(nums, start, mid - 1, target);
+        } else {
+          return mid;
+        }
+      } else if (nums[mid] < target) {
+        return binarySearch(nums, mid + 1, end, target);
+      }
+      return binarySearch(nums, start, mid - 1, target);
+    }
+}
+
+
+public class Solution {
+    /**
+     * @param nums: The integer array.
+     * @param target: Target to find.
+     * @return: The first position of target. Position starts from 0.
+     */
+    public int binarySearch(int[] nums, int target) {
+      // write your code here
+      return binarySearch(nums, 0, nums.length - 1, target);
+    }
+
+    private int binarySearch(int[] nums, int start, int end, int target) {
+      // base case 
+      if (start > end) {
+        return -1;
+      }
+
+      int mid = (start + end) / 2;
+      if (nums[mid] == target) {
+        while (mid >= 1 && nums[mid] == nums[mid - 1]) {
+          mid--;
+        }
+        return mid;
+      } else if (nums[mid] < target) {
+        return binarySearch(nums, mid + 1, end, target);
+      }
+      return binarySearch(nums, start, mid - 1, target);
+    }
+}
