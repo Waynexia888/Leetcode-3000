2a315ef807b46202a4d52442aa5b8947a90df4cd
Waynexia888 <qiwuwaynexia@gmail.com>
2022-03-12 14:15:33 -0800

    12 · Min Stack

---
+++ Lintcode/Queue & Stack/12 · Min Stack.java
@@ -0,0 +1,39 @@
+public class MinStack {
+    Stack<Integer> stack, minStack;
+    public MinStack() {
+        // do intialization if necessary
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+
+    /*
+     * @param number: An integer
+     * @return: nothing
+     */
+    public void push(int number) {
+        // write your code here
+        stack.push(number);
+        if (minStack.isEmpty() || number < minStack.peek()) {
+            minStack.push(number);
+        } else {
+            minStack.push(minStack.peek());
+        }
+    }
+
+    /*
+     * @return: An integer
+     */
+    public int pop() {
+        // write your code here
+        minStack.pop();
+        return stack.pop();
+    }
+
+    /*
+     * @return: An integer
+     */
+    public int min() {
+        // write your code here
+        return minStack.peek();
+    }
+}
