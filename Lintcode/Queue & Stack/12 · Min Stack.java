8ed2ac87d91f0e4802dcd358bd4c70e0fb9a1e44
Waynexia888 <qiwuwaynexia@gmail.com>
2022-03-12 14:31:21 -0800

    12 · Min Stack

--- Lintcode/Queue & Stack/12 · Min Stack.java
+++ Lintcode/Queue & Stack/12 · Min Stack.java
@@ -37,3 +37,46 @@ public class MinStack {
         return minStack.peek();
     }
 }
+
+// -------------------------有什么可以优化的地方吗？ 空间方面--------------------------
+
+public class MinStack {
+    Stack<Integer> stack, minStack;
+    public MinStack() {
+        // do intialization if necessary
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+
+    /*
+     * @param number: An integer
+     * @return: nothing
+     */
+    public void push(int number) {
+        // write your code here
+        stack.push(number);
+        if (minStack.isEmpty() || number <= minStack.peek()) {
+            minStack.push(number);
+        } 
+    }
+
+    /*
+     * @return: An integer
+     */
+    public int pop() {
+        // write your code here
+        int number = stack.pop();
+        if (number == minStack.peek()) {
+            minStack.pop();
+        }
+        return number;
+    }
+
+    /*
+     * @return: An integer
+     */
+    public int min() {
+        // write your code here
+        return minStack.peek();
+    }
+}
