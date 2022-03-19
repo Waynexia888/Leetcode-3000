2e4d6aed483d98ed7e767b12a4b571297b75ec77
Lin He <linhe@Lins-MacBook-Pro.local>
2022-03-18 21:46:05 -0400

    12 · Min Stack

--- Lintcode/Queue & Stack/12 · Min Stack.java
+++ Lintcode/Queue & Stack/12 · Min Stack.java
@@ -80,3 +80,51 @@ public class MinStack {
         return minStack.peek();
     }
 }
+
+//--------------------------solution----------------------------
+public class MinStack {
+    Stack<int[]> stack;
+    public MinStack() {
+        // do intialization if necessary
+        stack = new Stack<>();
+    }
+
+    /*
+     * @param number: An integer
+     * @return: nothing
+     */
+    public void push(int number) {
+        // write your code here
+        if (stack.isEmpty()) {
+            int[] item = new int[2];
+            item[0] = number;
+            item[1] = number;
+            stack.push(item);
+        } else {
+            int[] item = new int[2];
+            item[0] = number;
+            if (number > stack.peek()[1]) {
+                item[1] = stack.peek()[1];
+            } else {
+                item[1] = number;
+            }
+            stack.push(item);
+        }
+    }
+    //[2, 2], [3, 2], [1, 1]
+    /*
+     * @return: An integer
+     */
+    public int pop() {
+        // write your code here
+        return stack.pop()[0];
+    }
+
+    /*
+     * @return: An integer
+     */
+    public int min() {
+        // write your code here
+        return stack.peek()[1];
+    }
+}
