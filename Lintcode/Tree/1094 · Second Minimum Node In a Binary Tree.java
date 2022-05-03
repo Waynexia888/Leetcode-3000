fc27f95f26d528af7230b8808b302979f403796a
Waynexia888 <qiwuwaynexia@gmail.com>
2022-05-02 18:46:36 -0700

    1094 · Second Minimum Node In a Binary Tree

--- Lintcode/Tree/1094 · Second Minimum Node In a Binary Tree.java
+++ Lintcode/Tree/1094 · Second Minimum Node In a Binary Tree.java
@@ -82,3 +82,55 @@ public class Solution {
         topDownDfs(root.right, ans);
     }
 }
+
+// ----------------------------------------------------------------------
+
+/**
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
+ */
+
+public class Solution {
+    /**
+     * @param root: the root
+     * @return: the second minimum value in the set made of all the nodes' value in the whole tree
+     */
+    
+    public int findSecondMinimumValue(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>(2);
+        Set<Integer> set = new HashSet<>();   // 去重
+        queue.offer(root);
+        minHeap.offer(root.val);  // 维护一个大小为2的最小堆
+        set.add(root.val);
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                if (node.left != null && !set.contains(node.left.val)) {
+                    queue.offer(node.left);
+                    minHeap.offer(node.left.val);
+                    set.add(node.left.val);
+                }
+                if (node.right != null && !set.contains(node.right.val)) {
+                    queue.offer(node.right);
+                    minHeap.offer(node.right.val);
+                    set.add(node.left.val);
+                }
+            }
+        }
+        minHeap.poll();
+        if (minHeap.isEmpty()) {
+            return -1;
+        }
+        return minHeap.poll();
+    }
+}
